cbbfb6311a61d9fa72c4feeb1d23a0c71d67eb90
Matt Mortensen <mortensenm1205@gmail.com>
2018-03-06 01:10:58 -0700

    understanding sequelize migration files, trying to understand

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Todo;
+};
