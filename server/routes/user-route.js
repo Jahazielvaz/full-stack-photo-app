6567a893cf312cf9962e1370f1f149abf9045818
Matt Mortensen <mortensenm1205@gmail.com>
2018-03-08 17:54:24 -0700

    updating a little of my route to work with the signup.ejs

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -2,10 +2,10 @@ const userController = require('../controllers/user-controller.js');
 
 module.exports = (app) => {
   app.get('/signup', (req, res, next) => {
-    res.render('signup');
+    res.render('signup', {name: 'Matt'});
     next();
   });
 
-  app.post('/signup', userController.create)
+  app.post('/signup', userController.create);
 
 }
